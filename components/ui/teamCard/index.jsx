b5e2a5b4c922d8f702f4f4a5b4ddf637e3c13fc7
Dev-Golmohammadi <92379327+DEVMGO@users.noreply.github.com>
2023-03-15 10:53:18 +0330

    edit height image team card

--- components/ui/teamCard/index.jsx
+++ components/ui/teamCard/index.jsx
@@ -11,7 +11,7 @@ const TeamCard = ({ data }) => {
   return (
     <div
       // style={{ background: `url(${image?.src})` }}
-      className={`w-full 3xl:h-[538px] h-[448px] rounded-2xl overflow-hidden flex items-center justify-between flex-col relative ease-in-out transition-all duration-500 3xl:p-6 p-4 before:z-10 before:absolute before:bottom-0 before:left-0 before:w-full before:h-1/2 before:bg-opacity-60 before:bg-gradient-to-t before:from-gray-800 hover:before:from-white before:transition-all before:duration-500 before:ease-in-out group`}
+      className={`w-full 3xl:h-[538px] h-[500px] rounded-2xl overflow-hidden flex items-center justify-between flex-col relative ease-in-out transition-all duration-500 3xl:p-6 p-4 before:z-10 before:absolute before:bottom-0 before:left-0 before:w-full before:h-1/2 before:bg-opacity-60 before:bg-gradient-to-t before:from-gray-800 hover:before:from-white before:transition-all before:duration-500 before:ease-in-out group`}
     >
       <img src={data?.image?.src} alt={data?.name} className="absolute w-full h-full md:!object-cover !object-fill top-0 left-0" />
       <div className="w-full flex items-center justify-start z-20">
